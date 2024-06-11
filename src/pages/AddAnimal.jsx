0561b4695539f97792532728e91d264ce891d506
Eyleyn <91395048+Eyleyn@users.noreply.github.com>
2024-06-10 20:37:49 -0500

    Update AddAnimal.jsx

--- src/pages/AddAnimal.jsx
+++ src/pages/AddAnimal.jsx
@@ -4,6 +4,8 @@
                 Cantiller, Sophia Feona
                 BS in Computer Science - IV
                 June 2024
+    About the code:
+          Adds animal to the database.
 **/}
 
 import React, { useState } from "react";
