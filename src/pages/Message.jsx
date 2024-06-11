d1626fef5bcae6a252d57ba9c7d18dc2a1dbc25a
Eyleyn <91395048+Eyleyn@users.noreply.github.com>
2024-06-10 21:48:31 -0500

    Update Message.jsx

--- src/pages/Message.jsx
+++ src/pages/Message.jsx
@@ -4,6 +4,8 @@
                 Cantiller, Sophia Feona
                 BS in Computer Science - IV
                 June 2024
+    About the code:
+      Displays the conversation between the admin and the user.
 **/}
 
 import React, { forwardRef } from "react";
